e3ea9795a667383e1a9eb0afd6e8e36e2ff30d12
rahavdayan <rd57@njit.edu>
2025-02-02 19:27:27 -0500

    finished implementation of pinns network and placeholder functions

--- src/ferrofluid/diff_equation.py
+++ src/ferrofluid/diff_equation.py
@@ -13,7 +13,15 @@ def grad(outputs, inputs):
         outputs, inputs, grad_outputs=torch.ones_like(outputs), create_graph=True
     )
 
+# there is no analytic solution for the ferrofluid diffeq dx/dt = f(x), so this is just a placeholder function that returns t
+def position(t):
+    return t
 
-def target(mu0, B, M):
-    H = 1/mu0 * B - M
-    return H
+def magnetic_field(x):
+    return 1
+
+def magnetic_field_deriv(x):
+    return 1
+
+def magnetization(x):
+    return 1
